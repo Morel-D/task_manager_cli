dd2d5288f02180adb5059c6f516a9c60ae4bd14e
Morel-D <morelDen@gmail.com>
2025-09-27 23:02:50 +0100

    Type inference: match & datatype*

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,21 @@ fn main() {
     println!("2- List Task");
     println!("3- Mark as done");
     println!("4- Delete Task");
+    println!("5- Exit");
     println!("-------------------------------------");
-    println!("Select one Option: {}", input.trim());
+    println!("Select one Option: {}", input);
     io::stdin().read_line(&mut input).expect("Failed to read line");
+
+    let slice = input.trim().parse::<i32>().unwrap();
+
+    match slice {
+        1 => println!("Add task"),
+        2 => println!("List Task"),
+        3 => println!("Mark as done"),
+        4 => println!("Delete Task"),
+        5 => println!("Exit"),
+        _ => println!("Nothing"),
+
+    }
 
 }
